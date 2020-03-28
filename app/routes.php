2f67696949c6954cab18c8f5a5333ead434f1461
Bartosz Glanowski <bartoszglanowski@gmail.com>
2020-03-28 21:59:55 +0100

    Added new dependencies slim/psr7, slim/http. Changed slim version to 4.*, prepared simple hello world page

--- app/routes.php
+++ app/routes.php
@@ -1,6 +1,6 @@
 <?php
 $app->get('/home', function($request, $response){
-  return 'Home';
+  return $this->view->render($response, 'home.twig');
 });
 
 $app->get('/contact', function($request, $response){
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,11 +1,17 @@
 <?php
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+
 session_start();
 require __DIR__ . '/../vendor/autoload.php';
-$app = new \Slim\App([
-  'settings' => [
-    'displayErrorDetails' => true,
-  ]
-]);
+
+$app = AppFactory::create();
+
+$app->get('/', function (Request $request, Response $response, $args){
+  $response->getBody()->write("Hello World!");
+  return $response;
+});
 
 require __DIR__ . '/../app/routes.php';
 
@@ -13,4 +19,17 @@ $user = new \App\Models\User('Bartosz');
 echo 'Imie: '.$user->getName().'<br>';
 var_dump($user);
 die();
+
+$container = $app->getContainer();
+$container['view'] = function ($container){
+  $view = new \Slim\Views\Twig(__DIR__.'/../views',[
+  'cache' => false
+]);
+
+$view->addExtension(new \Slim\Views\TwigExtension(
+    $container->router,
+    $container->request->getUri()
+  ));
+  return $view;
+};
  ?>
