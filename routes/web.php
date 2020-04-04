dc897123b1756317f30faa62b9ef47269826e575
Dominik Kasprzak <kasprzakdominik@outlook.com>
2020-04-04 15:48:39 +0200

    Added providers

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,8 @@ use App\Support\View;
 
 Route::get('/', function(View $view){return $view('home');});
 
+Route::get('/home', function(View $view){return $view('home');});
+
 Route::get('/consultations', 'ConsultationsController@index');
 
 Route::get('/login', 'LoginController@index');
