24087451acfcdc5fd3fd0c62229d0ed0550ec515
Bartosz Glanowski <bartoszglanowski@gmail.com>
2020-04-06 13:12:50 +0200

    empty

--- app/Providers/ErrorMiddlewareServiceProvider.php
+++ app/Providers/ErrorMiddlewareServiceProvider.php
@@ -11,12 +11,12 @@ class ErrorMiddlewareServiceProvider extends ServiceProvider
     public function register()
     {
         $this->app->add(new WhoopsMiddleware());
-    //     $this->app->addErrorMiddleware(
-    //         config('middleware.error_details.displayErrorDetails'),
-    //         config('middleware.error_details.logErrors'),
-    //         config('middleware.error_details.logErrorDetails')
-    //     );
-    // }
+        // $this->app->addErrorMiddleware(
+        //     config('middleware.error_details.displayErrorDetails'),
+        //     config('middleware.error_details.logErrors'),
+        //     config('middleware.error_details.logErrorDetails')
+        // );
+    }
 
     public function boot()
     {
