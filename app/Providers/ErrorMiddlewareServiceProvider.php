38c3029400c3716cf6e96e86c6b0cf4dcc5561e5
Bartosz Glanowski <bartoszglanowski@gmail.com>
2020-04-06 14:01:03 +0200

    Added pretty whoops page error

--- app/Providers/ErrorMiddlewareServiceProvider.php
+++ app/Providers/ErrorMiddlewareServiceProvider.php
@@ -9,13 +9,8 @@ class ErrorMiddlewareServiceProvider extends ServiceProvider
 {
 
     public function register()
-    {
+    {   
         $this->app->add(new WhoopsMiddleware());
-        // $this->app->addErrorMiddleware(
-        //     config('middleware.error_details.displayErrorDetails'),
-        //     config('middleware.error_details.logErrors'),
-        //     config('middleware.error_details.logErrorDetails')
-        // );
     }
 
     public function boot()
