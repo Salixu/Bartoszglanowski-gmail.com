2ba9a968bcec05ff577458f6eb8e30fe9f69ac90
Bartosz Glanowski <bartoszglanowski@gmail.com>
2020-05-26 01:01:47 +0200

    Working on coflicts

--- app/routes.php
+++ app/routes.php
@@ -17,10 +17,10 @@ $app->get('/login', 'LoginController:getLogin')->setName('loginPage');
 $app->post('/login', 'LoginController:login');
 $app->get('/logout', "LoginController:logout");
 
-<<<<<<< HEAD
+
 $app->get('/adminConsultations', 'AdminController:getAdminPanel')->setName('adminPage');
 $app->post('/adminConsultations', 'AdminController:setVisit');
-=======
+
 $app->get('/forgotpassword/:id', function($id) use ($app) {
     $param = "secret-password-reset-code";
 
@@ -35,7 +35,7 @@ $app->get('/forgotpassword/:id', function($id) use ($app) {
     $mail->Subject = 'Instructions for resetting the password for your account with BadgerDating.com';
     $mail->Body    = "
         <p>Hi,</p>
-        <p>            
+        <p>
         Thanks for choosing BadgerDating.com!  We have received a request for a password reset on the account associated with this email address.
         </p>
         <p>
@@ -57,4 +57,3 @@ $app->get('/forgotpassword/:id', function($id) use ($app) {
         $app->halt(500);
     }
 });
->>>>>>> c93901be20a658014119bc239c785826346c6f88
